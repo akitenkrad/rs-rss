81ca0324c57eaac672cbfb765d750c1514148bf1
root <yusuke.akimoto@ariseanalytics.com>
2024-10-25 01:10:04 +0900

    update slack module

--- src/slack/mod.rs
+++ src/slack/mod.rs
@@ -2,8 +2,8 @@ use crate::sites::WebArticle;
 use chrono::Local;
 use dotenv::dotenv;
 use indicatif::ProgressBar;
-use keywords::rsc::{extract_keywords, load_keywords, Language};
-use serde_json::{json, Value};
+use keywords::rsc::{extract_keywords, load_keywords, Keyword, Language};
+use serde_json::json;
 use std::env;
 
 #[cfg(test)]
@@ -19,71 +19,76 @@ pub async fn notify_slack(
     let client = reqwest::Client::new();
     let slack_url = env::var("SLACK_WEBHOOK_URL").expect("SLACK_WEBHOOK_URL is not set");
     let kws = load_keywords();
-    let mut target_articles: Vec<(WebArticle, usize, Value)> = Vec::new();
+    let mut target_articles: Vec<(WebArticle, usize, Vec<Keyword>)> = Vec::new();
 
-    let mut index = 1;
-    let bar = ProgressBar::new(articles.len() as u64);
-    for article in articles.iter() {
-        if skip_outdated_articles && article.timestamp < now - chrono::Duration::days(1) {
-            continue;
-        }
-
-        let mut extracted_keywords =
-            extract_keywords(article.title.as_str(), kws.clone(), Language::Japanese);
-        extracted_keywords.extend(extract_keywords(
-            article.description.as_str(),
-            kws.clone(),
-            Language::Japanese,
-        ));
-        extracted_keywords.sort_by(|a, b| a.alias.cmp(&b.alias));
-        extracted_keywords.dedup_by(|a, b| a.alias == b.alias);
-
-        let payload = json!({
-            "attachments": [
-                {
-                    "color": "#36a64f",
-                    "pretext": format!("No.{} - {} @{}", index, article.site, article.timestamp.format("%Y.%m.%d")),
-                    "title": format!("{TITLE}",
-                        TITLE=article.title,
-                    ),
-                    "title_link": article.url,
-                    "text": format!("{DIVIDER}\nKEYWORDS: {SCORE}\n{KEYWORDS}\n{DIVIDER}\n{TEXT}",
-                        DIVIDER="-".repeat(75),
-                        SCORE=extracted_keywords.iter().map(|kwd| kwd.score).sum::<u8>(),
-                        KEYWORDS=extracted_keywords.iter().map(|kwd| kwd.alias.clone()).collect::<Vec<String>>().join(" / "),
-                        TEXT=article.description
-                    ),
-                }
-            ]
-        });
-        index += 1;
-        target_articles.push((
-            article.clone(),
-            extracted_keywords.iter().map(|kwd| kwd.score).sum::<u8>() as usize,
-            payload,
-        ));
-        bar.inc(1);
-    }
-    bar.finish();
-
-    target_articles.sort_by(|a, b| b.1.cmp(&a.1));
-    let bar = ProgressBar::new(target_articles.len() as u64);
-    for (_, _, payload) in target_articles.iter() {
-        let res = client
-            .post(&slack_url)
-            .header("Content-Type", "application/json")
-            .body(payload.to_string())
-            .send()
-            .await;
-
-        match res {
-            Ok(_) => {
-                bar.inc(1);
+    // collect articles and keywords
+    {
+        let bar = ProgressBar::new(articles.len() as u64);
+        for article in articles.iter() {
+            if skip_outdated_articles && article.timestamp < now - chrono::Duration::days(1) {
+                continue;
             }
-            Err(e) => eprintln!("Failed to send a message to Slack: {}", e),
+
+            let mut extracted_keywords =
+                extract_keywords(article.title.as_str(), kws.clone(), Language::Japanese);
+            extracted_keywords.extend(extract_keywords(
+                article.description.as_str(),
+                kws.clone(),
+                Language::Japanese,
+            ));
+            extracted_keywords.sort_by(|a, b| a.alias.cmp(&b.alias));
+            extracted_keywords.dedup_by(|a, b| a.alias == b.alias);
+
+            target_articles.push((
+                article.clone(),
+                extracted_keywords.iter().map(|kwd| kwd.score).sum::<u8>() as usize,
+                extracted_keywords,
+            ));
+            bar.inc(1);
         }
-        index += 1;
+        bar.finish();
     }
-    bar.finish();
+
+    // send messages to Slack
+    {
+        target_articles.sort_by(|a, b| b.1.cmp(&a.1));
+        let bar = ProgressBar::new(target_articles.len() as u64);
+        for (index, (article, _, kws)) in target_articles.iter().enumerate() {
+            let payload = json!({
+                "attachments": [
+                    {
+                        "color": "#36a64f",
+                        "pretext": format!("No.{} - {} @{}", index + 1, article.site, article.timestamp.format("%Y.%m.%d")),
+                        "title": format!("{TITLE}",
+                            TITLE=article.title,
+                        ),
+                        "title_link": article.url,
+                        "text": format!("{DIVIDER}\nKEYWORDS: {SCORE}\n{KEYWORDS}\n{DIVIDER}\n{TEXT}",
+                            DIVIDER="-".repeat(75),
+                            SCORE=kws.iter().map(|kwd| kwd.score).sum::<u8>(),
+                            KEYWORDS=kws.iter().map(|kwd| kwd.alias.clone()).collect::<Vec<String>>().join(" / "),
+                            TEXT=article.description
+                        ),
+                    }
+                ]
+            });
+
+            let res = client
+                .post(&slack_url)
+                .header("Content-Type", "application/json")
+                .body(payload.to_string())
+                .send()
+                .await;
+
+            match res {
+                Ok(_) => {
+                    bar.inc(1);
+                }
+                Err(e) => eprintln!("Failed to send a message to Slack: {}", e),
+            }
+        }
+        bar.finish();
+    }
+
     return Ok(());
 }
