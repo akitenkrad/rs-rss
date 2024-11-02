1094082b6d48b9a0d1520e4b30e5d680d09195de
akitenkrad <akitenkrad@gmail.com>
2024-11-02 13:32:51 +0900

    added new sites

---
+++ src/sites/mit_research/mod.rs
@@ -0,0 +1,58 @@
+use crate::sites::{Category, Html, Site, Text, WebArticle};
+use chrono::DateTime;
+use feed_parser::parsers;
+pub struct MITResearch {}
+
+#[cfg(test)]
+mod tests;
+
+#[async_trait::async_trait]
+impl Site for MITResearch {
+    fn name(&self) -> String {
+        return "MIT Research".to_string();
+    }
+    fn category(&self) -> super::Category {
+        return Category::News;
+    }
+    async fn get_articles(&self) -> Result<Vec<WebArticle>, String> {
+        let body = self
+            .request(&"https://news.mit.edu/rss/research".to_string())
+            .await;
+        let feeds = if let Ok(r) = parsers::rss2::parse(&body) {
+            r
+        } else {
+            return Err("Failed to parse RSS".to_string());
+        };
+        let mut articles = Vec::new();
+        for feed in feeds {
+            articles.push(WebArticle {
+                site: self.name(),
+                title: feed.title,
+                url: feed.link,
+                description: feed.description.unwrap_or("".to_string()),
+                timestamp: DateTime::parse_from_rfc2822(&feed.publish_date.unwrap())
+                    .unwrap()
+                    .into(),
+            });
+        }
+        return Ok(articles);
+    }
+    async fn get_article_text(&self, url: &String) -> Result<(Html, Text), String> {
+        let body = self.request(url).await;
+        let document = scraper::Html::parse_document(&body);
+        let selector =
+            scraper::Selector::parse("article div.news-article--content--body p").unwrap();
+        let text = document
+            .select(&selector)
+            .map(|x| x.text().collect::<Vec<_>>().join("\n"));
+        let html = document
+            .select(&selector)
+            .map(|x| x.html())
+            .collect::<Vec<_>>()
+            .join("\n");
+        return Ok((
+            self.trim_text(&html),
+            self.trim_text(&text.collect::<Vec<_>>().join("\n")),
+        ));
+    }
+}
---
+++ src/sites/tech_crunch/tests.rs
@@ -0,0 +1,33 @@
+use super::*;
+
+#[tokio::test]
+async fn test_tech_crunch() {
+    let site = TechCrunch {};
+    let articles = site.get_articles().await;
+    if let Ok(articles) = articles {
+        if articles.len() == 0 {
+            println!("No articles found");
+            assert!(true);
+            return;
+        }
+
+        let article = articles.get(0).unwrap();
+        println!("Article: {:?}", article);
+        let html_and_text = site.get_article_text(&article.url).await;
+        match html_and_text {
+            Ok(html_and_text) => {
+                let (html, text) = html_and_text;
+                println!("HTML: {}", html);
+                println!("Text: {}", text);
+                assert!(html.len() > 0);
+                assert!(text.len() > 0);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                assert!(false);
+            }
+        }
+    } else {
+        assert!(false);
+    }
+}
