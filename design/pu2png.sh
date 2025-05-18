c06dad7e9bac7f6f85f9f45e1f1ffdf98365f9f6
akitenkrad <akitenkrad@gmail.com>
2025-05-19 00:43:08 +0900

    update system design

--- design/pu2png.sh
+++ design/pu2png.sh
@@ -2,6 +2,6 @@
 set -eu
 
 java -jar design/plantuml.jar -charset UTF-8 design/class_diagram.pu
-java -jar design/plantuml.jar -charset UTF-8 design/erd.pu
+java -jar design/plantuml.jar -charset UTF-8 design/er.pu
 
-mv design/class_diagram.png design/erd.png design/images
+mv design/class_diagram.png design/er.png design/images
